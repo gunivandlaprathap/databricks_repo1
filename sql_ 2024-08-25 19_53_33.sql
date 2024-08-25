10a3c94d269d89fdda5f698caf656e296ac2ffb9
prathap.g10@gmail.com <prathap.g10@gmail.com>
2024-08-25 14:41:44 +0000

    update

---
+++ sql_ 2024-08-25 19_53_33.sql
@@ -0,0 +1,18 @@
+-- Databricks notebook source
+show databases
+
+-- COMMAND ----------
+
+use default;
+
+-- COMMAND ----------
+
+show tables in information_schema;
+
+-- COMMAND ----------
+
+select * from information_schema.tables;
+
+-- COMMAND ----------
+
+
